0f9e059c07bd480cdfaad27afbc98b10542e5143
c0a250169c <c0a250169c@edu.teu.ac.jp>
2026-06-16 14:24:30 +0900

    爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -10,12 +11,25 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")  
+
+    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 爆弾の初期化
+    bb_img = pg.Surface((20,20))  # 爆弾用のSurface
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 赤丸半径10
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0,WIDTH)  # 横初期座標
+    bb_rct.centery = random.randint(0,HEIGHT)  # 縦初期座標
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
+    
 
     DELTA={
         pg.K_UP:(0,-5),#上
@@ -46,7 +60,9 @@ def main():
                 sum_mv[1] += mv[1]#縦移動量
 
         kk_rct.move_ip(sum_mv)
+        bb_rct.move_ip(vx, vy)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
